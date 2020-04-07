461c96425660bf1c205cc6e10bcd6f2c2f29bb02
Johan Mazorra <johanmazorra@Claudias-MacBook-Air-3.local>
2020-04-07 01:20:21 -0400

    Package files locally and ignore artifacts from version control

---
+++ setup.py
@@ -0,0 +1,20 @@
+# setup.py file
+
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="my_lambdata", # the name that you will install via pip
+    version="1.0.0",
+    author="Johan Mazorra",
+    author_email="johan-mazorra@lambdastudents.com",
+    description="A short description",
+    long_description=long_description,
+    long_description_content_type="text/markdown", # required if using a md file for long desc
+    #license="MIT",
+    url="https://github.com/jsmazorra/my-lambdata",
+    #keywords="",
+    packages=find_packages() # ["my_lambdata"]
+)
